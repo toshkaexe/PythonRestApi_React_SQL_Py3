6159b6c229e57f36751ed1cce9388a2d22eb7580
AntonZ <anton.zeltser@retarus.de>
2021-03-21 10:31:32 +0100

    added db settings

--- fastapi/database.py
+++ fastapi/database.py
@@ -5,21 +5,23 @@ global sql
 db = sqlite3.connect('users.db')
 sql = db.cursor()
 sql.execute("""CREATE TABLE IF NOT EXISTS users(
+    username TEXT,
     login TEXT,
-    pass TEXT,
-    email TEXT
+    pass TEXT,    
+    email TEXT    
 )""")
 db.commit()
 
 
 def registation():
+    user_name = input('UserName: ')
     user_login = input('Login: ')
     user_password = input('Password: ')
-    email = input('Email: ')
-    sql.execute(f"SELECT login FROM users WHERE login='{user_login}'")
+    user_email = input('Email: ')
+    
     if sql.fetchone() is None:
         sql.execute(
-            f"INSERT INTO users VALUES('{user_login}', '{user_password}', '{email}')")
+            f"INSERT INTO users VALUES('{user_name}','{user_login}', '{user_password}', '{user_email}')")
         db.commit()
         print("registed")
     else:
@@ -27,4 +29,12 @@ def registation():
     for value in sql.execute("SELECT * FROM users"):
         print(value)
 
-registation()
+#registation()
+
+def getUserNameByLogin(sql):
+    login = input('Enter log: ')
+    for value in sql.execute(f"SELECT username FROM users WHERE login='{login}'"):
+        print(value)
+
+getUserNameByLogin(sql)
+
